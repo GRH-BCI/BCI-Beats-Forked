34ccd84e4e7ed9daa413d613228a9623af327da6
KaidenMastel <root@DESKTOP-BNPHA8Q>
2023-11-18 13:19:54 -0700

    Added music storage

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class PlayButton : MonoBehaviour
@@ -14,10 +15,19 @@ public class PlayButton : MonoBehaviour
     [SerializeField] public Button play2;
     [SerializeField] public Button play3;
 
+    [SerializeField] public Button choose1;
+    [SerializeField] public Button choose2;
+    [SerializeField] public Button choose3;
+
     // Start is called before the first frame update
     void Start()
     {
         play1.onClick.AddListener(OnButtonClick1);
+        play2.onClick.AddListener(OnButtonClick2);
+        play3.onClick.AddListener(OnButtonClick3);
+        choose1.onClick.AddListener(OnButtonClick4);
+        choose2.onClick.AddListener(OnButtonClick5);
+        choose3.onClick.AddListener(OnButtonClick6);
     }
 
     // Update is called once per frame
@@ -44,12 +54,51 @@ public class PlayButton : MonoBehaviour
     }
     private void OnButtonClick2()
     {
-        // Handle button click logic here
-        Debug.Log("Button Clicked!");
+        // Use the AudioSource component to play the audio clip
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // If there is no AudioSource component, add one
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Assign the audio clip to the AudioSource
+        audioSource.clip = melody2;
+
+        // Play the audio
+        audioSource.Play();
     }
     private void OnButtonClick3()
     {
-        // Handle button click logic here
-        Debug.Log("Button Clicked!");
+        // Use the AudioSource component to play the audio clip
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // If there is no AudioSource component, add one
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Assign the audio clip to the AudioSource
+        audioSource.clip = melody3;
+
+        // Play the audio
+        audioSource.Play();
+    }
+    private void OnButtonClick4()
+    {
+        StoreSounds.noises.Add(melody1);
+        SceneManager.LoadScene("Assets/Scenes/BandScreen.unity");
+    }
+    private void OnButtonClick5()
+    {
+        StoreSounds.noises.Add(melody2);
+        SceneManager.LoadScene("Assets/Scenes/BandScreen.unity");
+
+    }
+    private void OnButtonClick6()
+    {
+        StoreSounds.noises.Add(melody3);
+        SceneManager.LoadScene("Assets/Scenes/BandScreen.unity");
+
     }
 }
