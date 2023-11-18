98d2964f33b7b475af32558d6d49c33deedf0567
KaidenMastel <root@DESKTOP-BNPHA8Q>
2023-11-17 18:45:31 -0700

    Finished countdown

---
+++ Assets/Samples/BCI Essentials/1.0.0/Original P300 Controller/Scripts/Countdown.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+    private System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        watch.Start();
+
+        if (textMeshPro != null)
+        {
+            // Access and modify the text property
+            textMeshPro.text = "Hello, Unity!";
+        }
+        else
+        {
+            // Log an error if the TextMeshPro component is not assigned
+            Debug.LogError("TextMeshPro component is not assigned!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        textMeshPro.text = Math.Ceiling(((double)(9000 - watch.ElapsedMilliseconds)/1000)).ToString();
+
+        if (watch.ElapsedMilliseconds > 9000)
+        {
+            SceneManager.LoadScene("Assets/Scenes/ChooseInstrument.unity");
+        };
+
+    }
+}
