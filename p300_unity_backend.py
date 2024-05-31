fdff518f62213b5ffd7903817636e8965b3b7440
Nicholas <nicican6@gmail.com>
2024-05-31 14:42:00 -0600

    Able to now save training data + visualize/analyze in jupyter notebook

--- p300_unity_backend.py
+++ p300_unity_backend.py
@@ -8,7 +8,8 @@ from pylsl import StreamInlet, resolve_stream
 import pandas as pd
 import numpy as np
 from datetime import datetime
-#import threadingW
+import threading
+import mne
 
 
 # Add parent directory to path to access bci essentials
@@ -36,14 +37,80 @@ messenger = LslMessenger()
 test_erp = ErpData(classifier, eeg_source, marker_source, messenger)
 
 
+# Create eeg and marker streams
+print("looking for a marker and eeg streams...")
+marker_stream = resolve_stream('type', 'LSL_Marker_Strings')
+eeg_stream = resolve_stream('type', 'EEG')
+marker_inlet = StreamInlet(marker_stream[0])
+eeg_inlet = StreamInlet(eeg_stream[0])
 
-# # Connect the streams
-# test_erp.stream_online_eeg_data(timeout=5,
-#                            max_eeg_samples=1000000,
-#                            max_marker_samples=100000,
-#                            eeg_only=False,
-#                            subset=[])  # you can also add a subset here
-# test_erp.pull_data_from_stream(include_markers=False, include_eeg=True, return_eeg=False)
+global flag
+flag = True
+file_name = str(datetime.now().strftime("%Y-%m-%d %H-%M-%S"))
+path = './data_sets'
+if not os.path.exists(path):
+    os.mkdir(path)
+else:
+    print("Folder %s already exists" % path)
+
+
+
+
+def eeg_recorder():
+    global flag
+    eeg_channel_cnt = int(eeg_inlet.channel_count)
+    eeg_data = np.zeros((eeg_channel_cnt, 1))
+
+    eeg_timestamps = np.zeros((1,))
+    while flag:
+        eeg_sample, eeg_timestamp = eeg_inlet.pull_sample()
+        #print("got %s at time %s" % (eeg_sample, eeg_timestamp))
+        eeg_sample = np.array(eeg_sample).reshape(eeg_channel_cnt, 1)
+        eeg_timestamp = np.array(eeg_timestamp).reshape((1,))
+        eeg_data = np.concatenate((eeg_data, eeg_sample), axis=1)
+        eeg_timestamps = np.concatenate((eeg_timestamps, eeg_timestamp))
+    if not flag:
+        eeg_timestamps = eeg_timestamps.reshape((eeg_timestamps.shape[0], 1)).transpose()
+        channel_names = list(test_erp.channel_labels)
+        eeg_df = pd.DataFrame(np.concatenate((eeg_data, eeg_timestamps), axis=0).transpose(),
+                              columns=(channel_names + ['time_stamps']))
+        info = mne.create_info(ch_names=channel_names, sfreq=test_erp.fsample, ch_types='eeg')
+        raw_array = mne.io.RawArray(data=eeg_data, info=info)
+        #mne.export.export_raw(path + str('/' + file_name + '_EEG.edf'), raw_array, overwrite=False)
+        eeg_df.to_csv(path + str('/' + file_name + '_EEG.csv'), float_format='{:.7f}'.format)
+
+
+def marker_recorder():
+    global flag
+    marker_data = np.zeros((1,))
+    marker_timestamps = np.zeros((1,))
+    while flag:
+        marker_sample, marker_timestamp = marker_inlet.pull_sample()
+        #print("got %s at time %s" % (marker_sample[0], marker_timestamp))
+        if marker_sample[0] == 'Training Complete':
+            flag = False
+        if marker_sample[0] == 'Trial Started':
+            flag = True
+        marker_sample = np.array(marker_sample)
+        marker_data = np.concatenate((marker_data, marker_sample))
+        marker_timestamp = np.array(marker_timestamp).reshape((1,))
+        marker_timestamps = np.concatenate((marker_timestamps, marker_timestamp))
+    if not flag:
+        marker_data = marker_data.reshape((marker_data.shape[0], 1))
+        marker_timestamps = marker_timestamps.reshape((marker_data.shape[0], 1))
+        #print(marker_data.shape)
+        #print(marker_timestamps.shape)
+        marker_out = np.concatenate((marker_data, marker_timestamps), axis=1)
+        marker_df = pd.DataFrame(marker_out)
+        marker_df.to_csv(path + str('/' + file_name + '_markers.csv'), float_format='{:.7f}'.format)
+
+
+eeg_recorder_thread = threading.Thread(target=eeg_recorder, args=(), daemon=True)
+marker_recorder_thread = threading.Thread(target=marker_recorder, args=(), daemon=True)
+eeg_recorder_thread.start()
+marker_recorder_thread.start()
+
+
 
 
 # Run main
